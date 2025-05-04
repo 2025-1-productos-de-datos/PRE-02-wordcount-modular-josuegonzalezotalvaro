935b9cc3738b0448a9bd994e39d3e250629f43a4
Josué González Otálvaro <jgonzalezot@unal.edu.co>
2025-05-03 19:57:37 -0500

    Separación de funciones en archivos

--- homework/src/wordcount.py
+++ homework/src/wordcount.py
@@ -1,62 +1,21 @@
 # obtain a list of files in the input directory
-import os
 
+from ._internals.count_words import count_words
+from ._internals.preprocess_lines import preprocess_lines
+from ._internals.read_all_lines_1 import read_all_lines
+from ._internals.split_in_words import split_in_words
 from ._internals.write_count_words import write_count_words
 
 # Para traer la funcion que mandamos para otro archivo:
 
 
-# def read_all_lines():
-#     all_lines = []
-#     input_file_list = os.listdir("data/input/")
-#     for filename in input_file_list:
-#         with open(filename, "r", encoding="utf-8") as f:
-#             lines = f.readlines()
-#             all_lines.extend(lines)
-#     return all_lines
-
-
 def main():
 
-    ##
-    # input_file_list = os.listdir("data/input/")
-    # all_lines = read_all_lines()
-
-    ## read all lines
-    all_lines = []
-    input_file_list = os.listdir("data/input/")
-    for filename in input_file_list:
-        file_path = os.path.join("data/input", filename)
-        with open(file_path, "r", encoding="utf-8") as f:
-            lines = f.readlines()
-            all_lines.extend(lines)
-
-    ## preprocess lines
-    all_lines = [line.lower().strip() for line in all_lines]
-
-    ## split in words
-    words = []
-    for line in all_lines:
-        words.extend(word.strip(",.!?") for word in line.split())
-
-    ## count words
-    counter = {}
-    for word in words:
-        counter[word] = counter.get(word, 0) + 1
-
-    # count the frequency of the words in the files in the input directory
-    # counter = {}
-    # for filename in input_file_list:
-    #     with open("data/input/" + filename) as f:
-    #         for l in f:
-    #             for w in l.split():
-    #                 w = w.lower().strip(",.!?")
-    #                 counter[w] = counter.get(w, 0) + 1
-
-    ##
-    write_count_words(counter)  # Factorizado
-
-    # write_count_words(counter)
+    all_lines = read_all_lines()
+    all_lines = preprocess_lines(all_lines)
+    words = split_in_words(all_lines)
+    counter = count_words(words)
+    write_count_words(counter)
 
 
 if __name__ == "__main__":
